7cb001cb8f74c9ad12771e9ae31895249909fd29
Wanzhang He <745133601@qq.com>
2026-01-02 18:49:49 +0100

    Update models_mae.py

--- models_mae.py
+++ models_mae.py
@@ -14,10 +14,10 @@ from functools import partial
 import torch
 import torch.nn as nn
 
+from timm.models.vision_transformer import PatchEmbed, Block
+
 import inspect
 _HAS_QK_SCALE = "qk_scale" in inspect.signature(Block.__init__).parameters
-
-from timm.models.vision_transformer import PatchEmbed, Block
 
 from util.pos_embed import get_2d_sincos_pos_embed
 
